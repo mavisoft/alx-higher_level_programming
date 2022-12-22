61b73e82ae9d214ffb0e809c639d1029440d3123
mavisoft <iyoboiyoha@gmail.com>
2022-12-21 20:16:42 -0800

    updating functions in class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+""" A class Square that defines a square by: (based on 0-square.py)."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initialize a new square.
+        Args:
+            size (integer): The size of the square.
+        """
+        self.__size = size
